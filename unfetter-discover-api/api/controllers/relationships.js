bbfb1a821ea1209303ba035aa3d9a4bdada31ab5
Jennifer Burns <jburns@mitre.org>
2018-04-26 13:01:04 -0400

    Handle errors when source/target ref of relationship doesn't exist asynchronously

--- unfetter-discover-api/api/controllers/relationships.js
+++ unfetter-discover-api/api/controllers/relationships.js
@@ -30,74 +30,82 @@ module.exports = {
                     stixSchemaless.find({$or: [{_id: sourceId}, {_id: targetId}]}, (err, results) => {
                         if (results.length === 2) {
                             stixSchemaless.findById({ _id: targetId }, (err, origResult) => {
-                                let origResultObj = origResult.toObject();
-                                stixSchemaless.findById({ _id: sourceId }, (err, result) => {
-                                    const resultObj = result.toObject();
-                                    const metaProperty = {};
-                                    metaProperty.deleted = new Date();
-                                    metaProperty.created = deletedRelationship.stix.created;
-                                    metaProperty.ref = deletedRelationship.stix.target_ref;
-                                    metaProperty.id = deletedRelationship.stix.id;
-                                    metaProperty.name = origResultObj.stix.name
-                                    if (origResultObj.stix.external_references !== undefined) {
-                                        for (let i = 0; i < origResultObj.stix.external_references.length; i++) {
-                                            if (origResultObj.stix.external_references[i].external_id !== undefined) {
-                                                metaProperty.name = origResultObj.stix.external_references[i].external_id;
+                                if (origResult !== undefined) {
+                                    let origResultObj = origResult.toObject();
+                                    stixSchemaless.findById({ _id: sourceId }, (err, result) => {
+                                        if (result !== undefined) {
+                                            const resultObj = result.toObject();
+                                            const metaProperty = {};
+                                            metaProperty.deleted = new Date();
+                                            metaProperty.created = deletedRelationship.stix.created;
+                                            metaProperty.ref = deletedRelationship.stix.target_ref;
+                                            metaProperty.id = deletedRelationship.stix.id;
+                                            metaProperty.name = origResultObj.stix.name
+                                            if (origResultObj.stix.external_references !== undefined) {
+                                                for (let i = 0; i < origResultObj.stix.external_references.length; i++) {
+                                                    if (origResultObj.stix.external_references[i].external_id !== undefined) {
+                                                        metaProperty.name = origResultObj.stix.external_references[i].external_id;
+                                                    }
+                                                }
                                             }
+
+                                            if (resultObj.metaProperties !== undefined && resultObj.metaProperties.mitreId !== undefined) {
+                                                metaProperty.created_id = resultObj.metaProperties.mitreId;
+                                            }
+                                            if (req.user !== undefined && req.user.identity !== undefined && req.user.identity.id !== undefined, req.user.identity.name !== undefined) {
+                                            metaProperty.deleted_id = {'id': req.user.identity.id, 'name': req.user.identity.name};
+                                            }
+                                            if(resultObj.metaProperties === undefined){
+                                                resultObj.metaProperties = {};
+                                            }
+                                            if(resultObj.metaProperties['deletedRelationships'] === undefined) {
+                                                resultObj.metaProperties['deletedRelationships'] = [];
+                                            }
+                                            resultObj.metaProperties['deletedRelationships'].push(metaProperty);
+
+                                            stixSchemaless.findOneAndUpdate({ _id: sourceId }, resultObj, { new: true }, (errUpdate, resultUpdate) => {
+                                            });
                                         }
-                                    }
-
-                                    if (resultObj.metaProperties !== undefined && resultObj.metaProperties.mitreId !== undefined) {
-                                        metaProperty.created_id = resultObj.metaProperties.mitreId;
-                                    }
-                                    if (req.user !== undefined && req.user.identity !== undefined && req.user.identity.id !== undefined, req.user.identity.name !== undefined) {
-                                       metaProperty.deleted_id = {'id': req.user.identity.id, 'name': req.user.identity.name};
-                                    }
-                                    if(resultObj.metaProperties === undefined){
-                                        resultObj.metaProperties = {};
-                                    }
-                                    if(resultObj.metaProperties['deletedRelationships'] === undefined) {
-                                        resultObj.metaProperties['deletedRelationships'] = [];
-                                    }
-                                    resultObj.metaProperties['deletedRelationships'].push(metaProperty);
-
-                                    stixSchemaless.findOneAndUpdate({ _id: sourceId }, resultObj, { new: true }, (errUpdate, resultUpdate) => {
                                     });
-                                });
+                                }
                             });
                             stixSchemaless.findById({ _id: sourceId}, (err, origResult) => {
                                 let origResultObj = origResult.toObject();
-                                stixSchemaless.findById({ _id: targetId }, (err, result) => {
-                                    const resultObj = result.toObject();
-                                    const metaProperty = {};
-                                    metaProperty.deleted = new Date();
-                                    metaProperty.created = deletedRelationship.stix.created;
-                                    metaProperty.ref = deletedRelationship.stix.source_ref;
-                                    metaProperty.id = deletedRelationship.stix.id;
-                                    metaProperty.name = origResultObj.stix.name;
-                                    if (origResultObj.stix.external_references !== undefined) {
-                                        for (let i = 0; i < origResultObj.stix.external_references.length; i++) {
-                                            if (origResultObj.stix.external_references[i].external_id !== undefined) {
-                                                metaProperty.name = origResultObj.stix.external_references[i].external_id;
+                                if (origResult !== undefined) {
+                                    stixSchemaless.findById({ _id: targetId }, (err, result) => {
+                                        if (result !== undefined) {
+                                            const resultObj = result.toObject();
+                                            const metaProperty = {};
+                                            metaProperty.deleted = new Date();
+                                            metaProperty.created = deletedRelationship.stix.created;
+                                            metaProperty.ref = deletedRelationship.stix.source_ref;
+                                            metaProperty.id = deletedRelationship.stix.id;
+                                            metaProperty.name = origResultObj.stix.name;
+                                            if (origResultObj.stix.external_references !== undefined) {
+                                                for (let i = 0; i < origResultObj.stix.external_references.length; i++) {
+                                                    if (origResultObj.stix.external_references[i].external_id !== undefined) {
+                                                        metaProperty.name = origResultObj.stix.external_references[i].external_id;
+                                                    }
+                                                }
                                             }
+                                            if (resultObj.metaProperties !== undefined && resultObj.metaProperties.mitreId !== undefined) {
+                                                metaProperty.created_id = resultObj.metaProperties.mitreId;
+                                            }
+                                            if (req.user !== undefined && req.user.identity !== undefined && req.user.identity.id !== undefined, req.user.identity.name !== undefined) {
+                                            metaProperty.deleted_id = {'id': req.user.identity.id, 'name': req.user.identity.name};
+                                            }
+                                            if(resultObj.metaProperties === undefined){
+                                                resultObj.metaProperties = {};
+                                            }
+                                            if(resultObj.metaProperties['deletedRelationships'] === undefined) {
+                                                resultObj.metaProperties['deletedRelationships'] = [];
+                                            }
+                                            resultObj.metaProperties['deletedRelationships'].push(metaProperty);
+                                            stixSchemaless.findOneAndUpdate({ _id: targetId }, resultObj, { new: true }, (errUpdate, resultUpdate) => {
+                                            });
                                         }
-                                    }
-                                    if (resultObj.metaProperties !== undefined && resultObj.metaProperties.mitreId !== undefined) {
-                                        metaProperty.created_id = resultObj.metaProperties.mitreId;
-                                    }
-                                    if (req.user !== undefined && req.user.identity !== undefined && req.user.identity.id !== undefined, req.user.identity.name !== undefined) {
-                                       metaProperty.deleted_id = {'id': req.user.identity.id, 'name': req.user.identity.name};
-                                    }
-                                    if(resultObj.metaProperties === undefined){
-                                        resultObj.metaProperties = {};
-                                    }
-                                    if(resultObj.metaProperties['deletedRelationships'] === undefined) {
-                                        resultObj.metaProperties['deletedRelationships'] = [];
-                                    }
-                                    resultObj.metaProperties['deletedRelationships'].push(metaProperty);
-                                    stixSchemaless.findOneAndUpdate({ _id: targetId }, resultObj, { new: true }, (errUpdate, resultUpdate) => {
                                     });
-                                });
+                                }
                             });
                             return res.status(200).json({ data: { type: 'Success', message: `Deleted id ${id}` } });
                         } else {
