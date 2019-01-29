947ef88752c52057c47e9e1ab22e6a62e18f7e9a
Jennifer Burns <jburns@mitre.org>
2019-01-29 09:39:57 -0500

    Remedies issue #87

--- unfetter-discover-processor/processor.js
+++ unfetter-discover-processor/processor.js
@@ -202,7 +202,7 @@ function run(stixObjects = []) {
             }
             promises.push(stixModel.create(stixToUpload));
         }
-        if (extRefs.length > 0){
+        if (extRefs.length > 0 && argv.config !== undefined){
             let duplicates = []
             let uniqueRefs = extRefs.filter((citation, index, self) => self.findIndex((t) => (t.source_name === citation.source_name && t.description === citation.description && t.url === citation.url)) === index);
 
